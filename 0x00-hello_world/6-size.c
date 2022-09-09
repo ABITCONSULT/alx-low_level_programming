ddde37ed9986e8e4c5176206de6658422fb72a46
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-09 00:52:01 -0700

    cisfun

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ *main - main is the entry point of this program size
+ *
+ *Return:retuns zero if no error and non-zero if error
+*/
+int main(void)
+{
+	printf("Size of a char: %ld\n" sizeof(char));
+	printf("Size of an int: %ld\n" sizeof(int));
+	printf("Size of long int: %ld\n" sizeof(long int));
+	printf("Size of long long int: %ld\n" sizeof(long long int));
+	printf("Size of a float: %ld\n" sizeof(float));
+	return (0);
+}
