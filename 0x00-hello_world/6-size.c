de4ecc3dd99ee9333585e1832e661ab844a80a40
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-09 01:04:02 -0700

    cisfun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-	printf("Size of a char: %ld\n", sizeof(char));
-	printf("Size of an int: %ld\n", sizeof(int));
-	printf("Size of long int: %ld\n", sizeof(long int));
-	printf("Size of long long int: %ld\n", sizeof(long long int));
-	printf("Size of a float: %ld\n", sizeof(float));
+	printf("Size of a char: %ld\n", sizeof(char), "byte(s)");
+	printf("Size of an int: %ld\n", sizeof(int), "byte(s)");
+	printf("Size of long int: %ld\n", sizeof(long int), "byte(s)");
+	printf("Size of long long int: %ld\n", sizeof(long long int), "byte(s)");
+	printf("Size of a float: %ld\n", sizeof(float), "byte(s)");
 	return (0);
 }
