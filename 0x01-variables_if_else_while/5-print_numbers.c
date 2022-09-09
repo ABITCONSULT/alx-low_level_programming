777865092c9f4c28c4f71b6d2e72c6a99a947e1f
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-09 15:17:15 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is where our program starts and end
+ *
+ * Return:returns zero if no error and non zero if error
+*/
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10)
+		printf("%d");
+	printf("\n");
+	return (0);
