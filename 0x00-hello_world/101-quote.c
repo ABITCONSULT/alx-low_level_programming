5e125197da76008c10a8ae0c09159309142dbcf8
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-09 01:36:40 -0700

    cisfun

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point of the printing quote
+ *
+ * Return- returns zero if no error and non zero if error
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
