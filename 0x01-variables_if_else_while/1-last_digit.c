44648759a9d2889114b8c8ca9eebca549afaaaa9
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-09 12:31:25 -0700

    still on loops

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - this is where the program start running
+ *
+ * Return:returns zero if error and non zero if no errors
+*/
+int main(void)
+{
+	int n, l;
+		srand(time(0));
+			n = rand() - RAND_MAX / 2;
+		if (n > 5)
+			printf("Last digit of %n is %d and is greater than 5\n", n, l);
+		else
+			if (n == 0)
+				printf("Last digit of %d is %d and is 0\n", n, l);
+			else
+				printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the program run from here
+ *
+ * Return:return zero if no erro and non zero if error
+*/
+main()
+{
+	char alph = a;
+
+	while (a < z)
+	{
+		putchar("%c", alph);
+		alph++
+	}
+		putchar("\n");
+	return (0);
+}
