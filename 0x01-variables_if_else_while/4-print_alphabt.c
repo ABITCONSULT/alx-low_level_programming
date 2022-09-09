22500a24b89ca9d05ba12f5e77487e77ab83e57b
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-09 14:43:13 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - the program run from here
+ *
+ * Return:return zero if no erro and non zero if error
+*/
+int main(void)
+{
+	int alph;
+
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		if (alph == 'e')
+			alph++;
+		else
+			if (alph == 'q')
+				alph++;
+			else
+				putchar(alph);
+				alph++;
+	}
+	putchar('\n');
+	return (0);
+}
