4d6582befd9db214ac47572abbbfda357bb0bb8b
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-09 17:32:58 -0700

    really advanced

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,23 +13,23 @@ int main(void)
 		{
 			for (num2 = '0'; num2 <= '9'; num2++)
 			{
-				for (num3 = '0'; num3 <= '9'; num3++)
+			for (num3 = '0'; num3 <= '9'; num3++)
+			{
+				if (num1 < num2 && num2 < num3)
 				{
-					if (num1 < num2 && num2 < num3)
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
+					if (num1 != '7' && num2 != '8' && num3 != '9')
 					{
-						putchar(num1);
-						putchar(num2);
-						putchar(num3);
-						if (num1 == '7' && num2 == '8' && num3 == '9')
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					else
-					{
+						putchar(',');
+						putchar(' ');
 					}
 				}
+				else
+				{
+				}
+			}
 			}
 		num1++;
 		}
