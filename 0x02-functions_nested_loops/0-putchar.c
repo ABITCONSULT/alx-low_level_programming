80f35623b059996c525a1915e393d8518d834916
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-13 14:36:30 -0700

    headerwahala

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,9 +9,11 @@ int main(void)
 	int i = 0;
 	char out[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 
-	while (i < 8) {
+	while (i < 8) 
+	{
 		putchar(out[i]);
 		i++;
-			    }
+	}
+	putchar('\n')
 	return (0);
 }
