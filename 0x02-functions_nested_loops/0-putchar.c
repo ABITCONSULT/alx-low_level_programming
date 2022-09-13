3026de919188ae625a4bafc264616c46c1365602
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-13 08:25:46 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+nclude <stdio.h>                                                                                                         
+/**
+ * main - the main programs to print putch
+ *
+ * Return:returns 0
+*/
+main()
+{
+	printf("_putchar\n");
+	return(0)
+}
