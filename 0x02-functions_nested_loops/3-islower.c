0cbc0eafcef849120bd48ef2d6b28849b326c810
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-13 16:25:40 -0700

    _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - to check the case of alph
+ *
+*/
+int _islower(int c);
+{
+	int alph = 'a';
+	while (alph <= 'z')
+	{
+		if (c == alph)
+			return 1;
+	}
+	return 0;
+}
