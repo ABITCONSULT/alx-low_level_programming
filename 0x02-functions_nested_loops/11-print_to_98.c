e0f5161d7896176fb9cd6ab9e76508f56f928f59
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-14 11:57:17 -0700

    mult

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * print_to_98 - print all numbers from n to 98
- * @n number to start from
+ * @n:number to start from
  * Return:returns the listy of numbers
 */
 void print_to_98(int n)
