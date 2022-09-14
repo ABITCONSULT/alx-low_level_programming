86508ed591141a9fe42d088b71b6e9ea059709fc
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-14 11:56:13 -0700

    mult

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - print all numbers from n to 98
  * @n number to start from
@@ -6,61 +7,22 @@
 */
 void print_to_98(int n)
 {
-	int n1, n2;
-	if (n < 98)
+	if (n <= 98)
 	{
-	n1 = n + 1;
-	if (n <= 9)
-	_putchar((n % 10) + 48);
-	else 
-		if (n <= 99)
+		while (n < 98)
 		{
-		_putchar((n / 10) + 48);
-		_putchar((n % 10) + 48);
+			printf("%d, ", n);
+			n++;
 		}
-	else 
-		if (n > 99)
-		{
-		_putchar((n1 / 100) + 48);
-		n2 = n1 - ((n1/100) * 100);
-		_putchar((n2 / 10) + 48);
-		_putchar((n2 % 10) + 48);
-		}
-	while (n1 <= 98)
-	{
-		_putchar(',');
-		_putchar(' ');
-		if (n1 <= 9)
-		_putchar((n1 % 10) + 48);
-		else
-		{
-		_putchar((n1 / 10) + 48);
-		_putchar((n1 % 10) + 48);
-		}
-		n1++;
-	}
+	printf("98\n");
 	}
 	if (n > 98)
 	{
-		n1 = n - 1;
-		_putchar(n);
-		while (n1 >= 98)
+		while (n > 98)
 		{
-		_putchar(',');
-		_putchar(' ');
-		if (n1 <= 99)
-		{
-		_putchar((n1 / 10) + 48);
-		_putchar((n1 % 10) + 48);
+			printf("%d, ", n);
+			n--;
 		}
-		else
-		{
-		_putchar((n1 / 100) + 48);
-		n2 = n1 - ((n1/100) * 100);
-		_putchar((n2 / 10) + 48);
-		_putchar((n2 % 10) + 48);
-		}
-		n1--;
-		}
+	printf("98\n");
 	}
 }
