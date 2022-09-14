e4173de4be89a2d2163c0230f0cddf97716deed2
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-13 17:49:11 -0700

    _islower

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of an int
+ * @n:the number to get its abs value
+ * Return:returns absolute value
+*/
+int _abs(int n)
+{
+	int abs;
+
+	abs = ((n*n)**0.5);
+	return abs;
