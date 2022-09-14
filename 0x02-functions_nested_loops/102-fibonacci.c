24aee70cadee121baa1b67dae9fbe44af80132f0
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-14 16:24:38 -0700

    fibonnaci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,11 +6,11 @@
 */
 int main(void)
 {
-	int count, num1, num2, newnum;
+	long int count, num1, num2, newnum;
 	num1 = 1;
 	num2 = 2;
 	printf("%d, %d, ", num1, num2);
-	for (count = 0; count <= 50; count++)
+	for (count = 3; count <= 50; count++)
 	{
 	newnum = num1 + num2;
 	if (count < 50)
