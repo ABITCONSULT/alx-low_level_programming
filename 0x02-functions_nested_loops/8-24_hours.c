82a86d4c83ddfdc264446cae8518577ee51bfd5f
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-14 01:55:30 -0700

    jack buer time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,58 @@
+#include "main.h"
+/**
+ * jack_bauer - function that prints all the daily minutes
+ * @
+ * Return:returns void
+*/
+void jack_bauer(void)
+{
+	int H, h, M, m;
+
+	H = 0;
+	while (H <= 2)
+	{
+	if (H <= 1)
+	{
+		h = 0;
+		while (h <= 9)
+		{
+		M = 0;
+		while (M <= 5)
+		{
+		m = 0;
+		while (m <= 9)
+		{
+		_putchar(H);
+		_putchar(h);
+		_putchar(':');
+		_putchar(M);
+		_putchar(m);
+		_putchar('\n')
+		}
+		}
+		}
+	}
+	}
+	else
+	{
+		h = 0;
+		while (h <= 3)
+		{
+		M = 0;
+		while (M <= 5)
+		{
+		m = 0;
+		while (m <= 9)
+		{
+		_putchar(H);
+		_puthar(h);
+		_putchar(':');
+		_putchar(M);
+		_putchar(m);
+		_putchar('\n')
+		}
+		}
+		}
+	}
+	return (void);
+}
