0702f0d957d67798af0a5b8c4b1546ecdcb77cbf
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-14 14:31:23 -0700

    multiple5nd3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,17 @@
  * */ 
 int main(void) 
 {
-	int m3, m5, msum; 
+	int m3, m5, msum, msum1; 
 	m3 = 3; 
 	m5 = 5; 
+	msum1 = 0;
 	while (m3 < 1024)
 	{
 		while (m5 < 1024)
 			msum = m3 + m5;
 	m3 = m3 + 3;
 	m5 = m5 + 5;
-	msum = msum;
+	msum1 = msum1 + msum;
 	}
+printf("%d\n", msum1)
 }
