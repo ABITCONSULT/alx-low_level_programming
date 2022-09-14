538fb50e2dc758b032659f2db041cbc5c09d5018
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-14 15:42:20 -0700

    multiple5nd3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,15 +1,16 @@
-#include "main.h" 
-#include <stdio.h> 
-/** 
- * * Program that sums multiple of 3 an 5 to 1024
- * *  
- * * Retun:returns the value of sum
- * */ 
-int main(void) 
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Program that sums multiple of 3 an 5 to 1024
+ *
+ * Retun:returns the value of sum
+*/
+int main(void)
 {
-	int m3, m5, i, msum; 
-	m3 = 3; 
-	m5 = 5; 
+	int m3, m5, i, msum;
+
+	m3 = 3;
+	m5 = 5;
 	msum = 0;
 	for (i = 0; i < 1024; i++)
 	{
