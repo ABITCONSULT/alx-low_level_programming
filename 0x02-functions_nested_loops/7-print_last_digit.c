376f401f3439b20384f8a4969a81b989113442a5
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-13 18:42:09 -0700

    ld

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digi of a code
+ * @n: is the digit to be tested
+ * Return:returns the last digit
+*/
+int print_last_digit(int n)
+{
+	int last_digit
+	
+	last_digit = n % 10
+	return (last_digit)
+}
