c9080d306c101ea2d02d9cd462a01f635fdae0d4
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-14 10:53:10 -0700

    mult

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 - print all numbers from n to 98
+ * @n number to start from
+ * Return:returns the listy of numbers
+*/
+void print_to_98(int n)
+{
+	int n1;
+
+	n1 = n + 1;
+	_putchar(n);
+	while (n1 <= 98)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n1);
+		n1++
+	}
+}
