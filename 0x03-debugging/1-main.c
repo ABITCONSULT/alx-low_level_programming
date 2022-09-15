c62ed38ea9b0b5d63cbe446e60f6fc9fe1c0cf79
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-15 02:48:07 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-/**
-* while (i < 10)
-* {
-* putchar(i);
-* } no increment for i so i will always be < 10
-*/
+	/**
+	* while (i < 10)
+	* {
+	* putchar(i);
+	* } no increment for i so i will always be < 10
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
