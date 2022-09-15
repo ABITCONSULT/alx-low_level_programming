c19dc107ab42ef3c005851ad8c0d978341d248bd
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-15 01:11:57 -0700

    fibo

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 /**
  * main - program 50 fibonacci starts here
- * @n
+ * and
  * Return: returns 0 (success)
 */
 int main(void)
 {
-		long long num1, num2, newnum;
+		double num1, num2, newnum;
 		int count;
 
 		num1 = 1;
 		num2 = 2;
-		printf("%lld, %lld, ", num1, num2);
+		printf("%g, %g, ", num1, num2);
 		for (count = 1; count <= 96; count++)
 		{
 		newnum = num1 + num2;
 		if (count < 96)
-		printf("%lld, ", newnum);
+		printf("%g, ", newnum);
 		else
-		printf("%lld\n", newnum);
+		printf("%g\n", newnum);
 		num1 = num2;
 		num2 = newnum;
 		}
