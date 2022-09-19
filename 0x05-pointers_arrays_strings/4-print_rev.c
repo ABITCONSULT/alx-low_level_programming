cdb6c527fad94154833e8b1441a0ede6d3caab48
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-19 14:52:09 -0700

    arraystart

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - to print string reversed
+ * @s: the string to print reversed
+ * Return: returns void
+ */
+void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s + 0);
+		s++;
+	}
+	_putchar('\n);
+}
