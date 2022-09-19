e74f2152f8fe76fd6a3668c80ae72df2c29e91c6
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-19 13:58:31 -0700

    arraystart

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the lenght of a string
+ * @s the pointer to the string to print
+ * Return:returns int count
+ */
+int _strlen(char *s)
+{
+	int count1, len;
+	char str1;
+	
+	str1[len] = *s;
+	for (count1 = 0; count1 < len; count1++)
+	{
+	}
+	_putchar(count1 + 48);
+	return (count1);
+}
