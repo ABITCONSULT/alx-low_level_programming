53c6e8a27fdab37c1cfc93f7df8cb774769b0b1e
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-19 15:52:19 -0700

    arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,13 @@ void print_rev(char *s)
 	int index;
 
 	for (index = 0; s[index] != '\0'; ++index)
-		for (--index; index >= 0; --index)
+	{
+		index = index + 0;
+	}
+		while (index >= 0)
+		{
 			_putchar(s[index]);
+			index--;
+		}
 	_putchar('\n');
 }
