a65bb0d500b777b0414d6b631ffc004ac2ddc06f
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-19 12:56:37 -0700

    arraystart

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value stored in a and b
+ * @a: first value to swap for b
+ * @b: second value to swap for a
+ * Return: returns void on success
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
