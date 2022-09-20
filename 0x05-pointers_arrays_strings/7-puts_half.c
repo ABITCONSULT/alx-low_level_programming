5835c573f69f6c97d774326785abe2fdd552ff18
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-20 10:45:34 -0700

    arrayss

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - prints a string to half
+ * @str: string to print to half
+ * Return: returns void
+ */
+void puts_half(char *str)
+{
+	int c1, c2;
+
+	for (c1 = 0; str[c1] != 0; c1++)
+	{
+	}
+	for (c2 = 0; c2 > (c1 / 2) c2++)
+	{
+		_putchar(str[c2]);
+	}
+	_putchar('\n');
+}
