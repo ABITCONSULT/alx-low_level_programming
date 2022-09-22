ad341a8af9fbcc4b5f488132c9c53c6bbfc7748f
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-22 15:29:22 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,29 +1,20 @@
 #include "main.h"
 
 /**
- * cap_string - changes lower to upper case
- * @p:string to check for lower to upper
- * Return: returns p upper
+ * string_toupper - changes all lower case to upper case char
+ * @p: character to check for change
+ * Return: returns p
  */
-char *cap_string(char *p)
+char *string_toupper(char *p)
 {
 	int i;
 
 	for (i = 0; *(p + i) != '\0'; i++)
 	{
-		if ((*(p + i - 1) <= 122 && *(p + i - 1) >= 97) ||  (*(p + i - 1) <= 90 && *(p + i - 1) >= 65))
-		{
-		}
-		else if (*(p + i - 1) <= 57 && *(p + i - 1) >= 48)
-		{
-		}
+		if (*(p + i) <= 122 && *(p + i) >= 97)
+			*(p + i) = *(p + i) - 32;
 		else
 		{
-			if (*(p + i) <= 122 && *(p + i) >= 97)
-				*(p + i) = *(p + i) - 32;
-			else
-			{
-			}
 		}
 	}
 	return (p);
