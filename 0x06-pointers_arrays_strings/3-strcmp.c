abcee628b37039003c41774b3587f4ab296d9e3c
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-22 14:59:57 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/*
- * _strcmp - copies strings
+/**
+ * _strcmp - compares string s1 and s2
  * @s1: string to compare 1
  * @s2: string to compare 2
  * Return: returns value of difference
@@ -9,7 +9,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int value;
-		
+
 	while (*s1 != '\0')
 	{
 		if (*s1 == *s2)
