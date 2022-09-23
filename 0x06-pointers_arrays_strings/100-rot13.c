80c60c3f3c8dfcd610e249791996d259f0566023
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-22 18:09:52 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rot13 - rotates character by 13 places
+ * @p: the array to rotate its character
+ * Return: returns p
+ */
+char *rot13(char *p)
+{
+	while (*p != '\0')
+	{
+		if ((*p <= 77 && *p >= 65) || (*p <= 109 && *p >= 97))
+			*p = *p + 13;
+		else if ((*p <= 90 && *p >= 78) || (*p <= 122 && *p >= 110))
+			*p = *p - 13;
+	}
+	return (p);
+}
