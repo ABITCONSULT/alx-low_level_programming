eb7b30c15daaad0de1571ee126ff59be744d3415
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-22 17:02:29 -0700

    4

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int c1 = 0, c2 = 0;
 
-	while (*dest != '\0')
+	while (*(dest + c1) != '\0')
 	{
-		dest++;
+		c1++;
 	}
-	while (*src != '\0')
+	while (*(src + c2) != 0)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		*(dest + c1) = *(src + c2);
+		c1++;
+		c2++;
 	}
 	*dest = *src;
 	return (dest);
