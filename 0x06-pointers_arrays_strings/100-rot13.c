cecdde7cb69d91157e3ae0873e8bc9724e59a18c
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-22 18:44:10 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,21 +7,21 @@
  */
 char *rot13(char *p)
 {
-	int i,j;
-char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i, j;
+	char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-for (i = 0; p[i] != '\0'; i++)
-{
-	for (j = 0; x[j] != '\0'; j++)
+	for (i = 0; p[i] != '\0'; i++)
 	{
-		if (p[i] == x[j])
+		for (j = 0; x[j] != '\0'; j++)
 		{
+			if (p[i] == x[j])
+			{
 			p[i] = y[j];
 			break;
+			}
 		}
 	}
-}
 
-return (s);
+	return (p);
 }
