ae364c989b6783e095d0194243f484a52524fabf
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-26 16:37:54 -0700

    omemset

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory from src to dest
+ * @src: memory to copy from
+ * @dest: mempry to copy to
+ * @n: number of memory to copy
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(src + i) = *(dest + i);
+	}
+	return (dest);
+}
