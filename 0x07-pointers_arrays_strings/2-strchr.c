271913a51edffe9dc72dd8f6a0132c6e3891288e
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-26 17:31:27 -0700

    search str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - search for a character in a string
+ * @s: the string to search from
+ * @c: the charcter to search for
+ * Return: returns s at c
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+			break;
+		}
+		else if (*(s + 1) = '\0')
+			return ('\0');
+	s++;
+	}
+}
