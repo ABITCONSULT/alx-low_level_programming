ff75ba4bd83ad72bf3a9e38b4234e0ed8e603743
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-28 15:48:09 -0700

    factorial

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the power of x as y
+ * @x: the base number to multiply recurisively
+ * @y: the number of times to multiply x
+ * Return: returns x^y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y = 0)
+		return (1);
+	else if (y > 0)
+		y--;
+		return (x * _power_recursion(x, y));
+}
