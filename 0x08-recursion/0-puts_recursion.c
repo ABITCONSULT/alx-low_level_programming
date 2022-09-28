fb5a7c807d1259c98204578b5fb864ce18fbd5b9
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-28 13:29:04 -0700

    recurssion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: the string to print
+ * Return: return zero on success
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s - 1);
+	}
+}
