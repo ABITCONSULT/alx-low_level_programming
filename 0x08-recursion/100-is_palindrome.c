9eaae65207d110045f408e15bdfdd93314ba8526
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-29 03:14:37 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -29,7 +29,7 @@ int rev(char *s, int n1, int n2)
 
 	if (s[n1] == s[n2])
 	{
-		if (n2 >= 0)
+		if (n1 < n2)
 		{
 		n1++;
 		n2--;
@@ -52,5 +52,5 @@ int is_palindrome(char *s)
 	if (*s == '\0')
 		return (0);
 	return (rev(s, 0, (len(s) - 1)));
-	
+
 }
