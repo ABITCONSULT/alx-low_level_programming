71661553856aca8d1b4d77ca1295ebb2cc281f57
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-30 12:54:29 -0700

    argc&argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,13 @@
 
 /**
  * main - prints the program name
- *
+ * @argc: the lenght of array in argv
+ * @argv: the array of command in the command
  * Return: returns zero upon success
  */
 int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
+	return (0);
 }
