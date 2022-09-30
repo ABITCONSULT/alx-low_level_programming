7f72b6b03ae3d0c5899da2be4f49042a7d1c3477
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-30 15:46:09 -0700

    argc&argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - gives cent change
+ * @argc: to check number of arguments
+ * @argv: the array of argument entered
+ * Return: returns 0 upon succes
+ */
+int main(int argc, char *argv[])
+{
+	int change = 0, value;
+
+	value = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		while (value != 0)
+		{
+		if (value < 0)
+			change = 0;
+		else if (value >= 25)
+		{
+			change += value / 25;
+			value = value % 25;
+		}
+		else if (value >= 10)
+		{
+			change += value / 10;
+			value = value % 10;
+		}
+		else if (value >= 5)
+		{
+			change += value / 5;
+			value = value % 5;
+		}
+		else if (value >= 2)
+		{
+			change += value / 2;
+			value = value % 2;
+		}
+		else
+		{
+			change += value / 1;
+			value = value % 1;
+		}
+		}
+	printf("%d\n", change);
+	return (0);
+	}
+}
