70226e40d8c07a03f03a2f095a1fd2b75a01adb6
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-09-30 17:02:18 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	int change = 0, value;
 
 	value = atoi(argv[1]);
-	if (argc != 2)
+	if (argc != 2; || !isdigit(*(argv[1])))
 	{	printf("Error\n");
 		return (1);
 	}
