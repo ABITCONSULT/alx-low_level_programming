109d6466299119a66d27be02ded19cf26bde68f6
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-03 12:17:01 -0700

    nalloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates array ofa given cahr with malloc
+ * @size: size of th arry to create
+ * @c: the character to fill
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *p;
+
+	p = malloc(sizeof(c) * size);
+	i = 0;
+	while (i < size)
+	{
+		p[i] = c;
+		i++;
+	}
+	return (p);
+}
