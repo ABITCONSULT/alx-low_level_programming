82c01ceed1518bfad04059d674854aadd48c17a7
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-04 12:38:38 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,6 +3,44 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - prints the lenght of a string
+ * @s:the pointer to the string to print
+ * Return:returns int count
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		count = count + 1;
+		s = (s + 1);
+	}
+	return (count);
+}
+
+/**
+ * _strcpy - copies astring from one str to naother dest
+ * @src: the original string to copy
+ * @dest: the holder to copy to
+ * Return:returns dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int c1, c2 = 0;
+
+	for (c1 = 0; src[c1] != '\0'; c1++)
+	{
+		dest[c2] = src[c1];
+		c2++;
+	}
+	dest[c2] = '\0';
+	return (dest);
+}
+
+
+/**
  * _strdup - returns a pointer to the copy of a string in param
  * @str: the given string to copy
  * Return: returns the pointer to the copy of the string
@@ -10,28 +48,20 @@
 char *_strdup(char *str)
 {
 	char *copy;
-	unsigned int j;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (str == NULL)
 		return (NULL);
 	
-	while (*str != '\0')
-	{
-		i++;
-		str++;
-	}
+
+	i = _strlen(str);
 
 	copy = (char *)malloc((i + 1) * sizeof(char));
 
 	if (copy == NULL)
 		return (NULL);
 
-	for (j = 0; str[j] != 0; j++)
-	{
-		copy[j] = str[j];
-	}
-	copy[j] = '\0';
-
+	_strcpy(copy str);
+	
 	return (copy);
 }
