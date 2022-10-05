905263571e30a473c964c7426fbcf6d49147581a
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-05 02:18:24 -0700

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,9 +15,24 @@ int **alloc_grid(int width, int height)
 
 	if (width == 0 || height == 0)
 		return (NULL);
-	array = malloc((width * height) * sizeof(int));
+	array = malloc(height * sizeof(int));
+	if (array == NULL)
+	{
+		free(array);
+		return (NULL);
+	}
 
-
+	for (i =0 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] = NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(array[i]);
+		free(array);
+		return (NULL);
+		}
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
