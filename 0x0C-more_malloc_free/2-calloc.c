537ba6b7fe8b4272ff827ac92cd7e4a9770092d3
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-06 15:02:34 -0700

    mall

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - creates memory for array
+ * @nmemb: number of member of the array
+ * @size: size of the data type
+ * Return: returns void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
+
+	return (p);
+}
