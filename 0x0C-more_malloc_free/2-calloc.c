56716c80e6d82dd626717da0ab6845bf437da008
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-06 15:17:53 -0700

    mall

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,6 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
 	void *p;
 
 	if (nmemb == 0 || size == 0)
@@ -19,9 +18,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(size * nmemb);
 	if (p == NULL)
 		return (NULL);
-
-	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
 
 	return (p);
 }
