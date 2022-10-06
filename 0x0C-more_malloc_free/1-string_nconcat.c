b372bb755a695896e096ee9ac205c4ff0c7a94bc
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-06 14:06:34 -0700

    mallocation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, k, l;
 	char *p;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	i = strlen(s1);
 	j = strlen(s2);
 
