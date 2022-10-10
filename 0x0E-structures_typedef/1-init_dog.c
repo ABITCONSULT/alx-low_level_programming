75e4e308924fe76d3e5beb00068ccb88e9c76434
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-10 14:08:56 -0700

    doginit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include "main.h"
 
 /**
  * init_dog - initializes values for dog
@@ -6,6 +7,7 @@
  * @age: age of dog
  * @owner: owner of dog
  * @d: pointer to dog
+ * Return: returns d
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
