8c8bb8ab9fb006cd14e2b049f0eaa646b598c6be
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-10 12:52:22 -0700

    structdog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,18 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - describes a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
