8d1ed48e36419084f32696a60e2752a16353374b
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-13 09:43:22 -0700

    funcpointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name 
+ * @name: name to print
+ * @f: function to pointer to apply
+ * Return: returns void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
