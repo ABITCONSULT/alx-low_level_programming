7f6179300bddbaa8379208b6082e44a0b6abe1ff
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-13 09:56:04 -0700

    funcpointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints the name 
+ * print_name - prints the name
  * @name: name to print
  * @f: function to pointer to apply
  * Return: returns void
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - performs function action on all array member
+ * @array: array to perform on
+ * @size: size of the array
+ * @action: pointer to function to perform on array
+ * Return: returns void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
