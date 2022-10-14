053af7e9790ca3ef4f965c0b6bc3e0259a8e31cf
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-14 04:02:40 -0700

    varfunprint

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print all the argument numbers with separator
+ * @separator: what separates the numbers
+ * @n: total number of numbers to be printed
+ * Return:returns nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	va_list(num);
+
+	for (i = 0, i < n; i++)
+	{
+		printf("%d", va_arg(num int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(num);
+}
