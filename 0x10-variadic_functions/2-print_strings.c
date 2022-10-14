9cf7e288d0f6405876920e520252475b6f5cc828
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-14 05:11:27 -0700

    varfunprint

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints the string arg with separator
+ * @separator: separator that separates the strings
+ * @n: number of strings to print
+ * Return: returns nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list str;
+	unsigned int i;
+
+	var_start(str, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(str))
+			printf("%s", va_arg(str));
+		else
+			printf("(nil)");
+
+		if (separator && i < (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(str);
+}
