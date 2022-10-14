efe9a4a16170a526610d6b878dd83d54fdee8c03
ABIOLA TOLULOPE <abiolato92@gmail.com>
2022-10-14 05:24:12 -0700

    varfunprint

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(strr, char *)
+		str = va_arg(strr, char *);
 
 		if (str)
 			printf("%s", str);
